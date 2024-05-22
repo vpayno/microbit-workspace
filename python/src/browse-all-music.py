382aa03c2935a61dc17d6a0f3d78e6819467b9ab
Victor Payno <vpayno@users.noreply.github.com>
2024-05-21 20:56:09 -0700

    python: update music browser experiment

--- python/src/browse-all-music.py
+++ python/src/browse-all-music.py
@@ -2,50 +2,121 @@
 import music as Music
 from microbit import *
 
-songs: "list[str]" = [music for music in dir(Music) if music.isupper()]
-index: int = -1
-COUNT: int = len(songs)
+
+class MusicBrowser:
+    # Music icon
+    LOGO: Image = Image.MUSIC_QUAVERS
+
+    PLAY: Image = Image(
+            "00000:"
+            "09900:"
+            "09090:"
+            "09900:"
+            "00000:"
+    )  # fmt: off
+
+    PAUSE: Image = Image(
+            "00000:"
+            "09090:"
+            "09090:"
+            "09090:"
+            "00000:"
+    )  # fmt: off
+
+    STOP: Image = Image(
+            "00000:"
+            "09990:"
+            "09090:"
+            "09990:"
+            "00000:"
+    )  # fmt: off
+
+    FWD: Image = Image(
+            "00000:"
+            "90090:"
+            "99099:"
+            "90090:"
+            "00000:"
+    )  # fmt: off
+
+    BWD: Image = Image(
+            "00000:"
+            "09009:"
+            "99099:"
+            "09009:"
+            "00000:"
+    )  # fmt: off
+
+    SOUND: Image = Image(
+            "00090:"
+            "99009:"
+            "99009:"
+            "99009:"
+            "00090:"
+    )  # fmt: off
+
+    def __init__(self) -> None:
+        """Initialize MusicBrowser."""
+
+        display.show(self.LOGO)
+        sleep(1_000)
+
+        self.index: int = 0
+        self.songs: "list[str]" = [music for music in dir(Music) if music.isupper()]
+        self.song: str = self.songs[self.index]
+        self.size: int = len(self.songs)
+
+        print("INFO: songs=" + str(self.songs))
+
+    def browse_left(self) -> None:
+        """Scroll carousel to the left."""
+
+        self.index = (self.index - 1) % self.size
+        self.song = self.songs[self.index]
+
+        self.show()
+
+    def browse_right(self) -> None:
+        """Scroll carousel to the right."""
+
+        self.index = (self.index + 1) % self.size
+        self.song = self.songs[self.index]
+
+        self.show()
+
+    def show(self) -> None:
+        """Show the current song."""
+
+        print("INFO: song: " + self.song)
+        display.scroll(self.song)
+
+    def play(self) -> None:
+        """Play the current song."""
+
+        display.show(self.SOUND)
+        Music.play(getattr(Music, self.song))
 
 
-def browse_left(index: int) -> int:
-    """Scroll carousel to the left.
+print()
+print("Music Browser")
+print()
 
-    :param index: the current position
-    :return: the new position
-    """
-    return (index - 1) % COUNT
-
-
-def browse_right(index: int) -> int:
-    """Scroll carousel to the right.
-
-    :param index: the current position
-    :return: the new position
-    """
-    return (index + 1) % COUNT
-
+app: MusicBrowser = MusicBrowser()
 
 # Code in a 'while True:' loop repeats forever
 while True:
-    display.show(Image.ARROW_E)
+    display.show(app.BWD)
     sleep(500)
-    display.show(Image.ARROW_W)
+    display.show(app.FWD)
+    sleep(500)
+
+    app.show()
+    sleep(500)
+
+    app.play()
     sleep(500)
 
     if button_a.was_pressed():
-        index = browse_left(index)
+        app.browse_left()
     elif button_b.was_pressed():
-        index = browse_right(index)
-
-    song: str
-
-    if index == -1:
-        song = "BA_DING"
-    else:
-        song = songs[index]
-
-    print("INFO: song: " + song)
-    display.scroll(song)
-    Music.play(getattr(Music, song))
-
-    sleep(1_000)
+        app.browse_right()
