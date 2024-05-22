d2fee3b3bf1ee855c079b53d90619de6ed6089ef
Victor Payno <vpayno@users.noreply.github.com>
2024-05-21 21:03:30 -0700

    python: update sound browser experiment

--- python/src/browse-all-sounds.py
+++ python/src/browse-all-sounds.py
@@ -1,50 +1,123 @@
 # Imports go at the top
 from microbit import *
 
-sounds: "list[str]" = [sound for sound in dir(Sound) if sound.isupper()]
-index: int = -1
-COUNT: int = len(sounds)
+
+class SoundBrowser:
+    # Music icon
+    LOGO: Image = Image.MUSIC_QUAVERS
+
+    PLAY: Image = Image(
+            "00000:"
+            "09900:"
+            "09090:"
+            "09900:"
+            "00000:"
+    )  # fmt: off
+
+    PAUSE: Image = Image(
+            "00000:"
+            "09090:"
+            "09090:"
+            "09090:"
+            "00000:"
+    )  # fmt: off
+
+    STOP: Image = Image(
+            "00000:"
+            "09990:"
+            "09090:"
+            "09990:"
+            "00000:"
+    )  # fmt: off
+
+    FWD: Image = Image(
+            "00000:"
+            "90090:"
+            "99099:"
+            "90090:"
+            "00000:"
+    )  # fmt: off
+
+    BWD: Image = Image(
+            "00000:"
+            "09009:"
+            "99099:"
+            "09009:"
+            "00000:"
+    )  # fmt: off
+
+    SOUND: Image = Image(
+            "00090:"
+            "99009:"
+            "99009:"
+            "99009:"
+            "00090:"
+    )  # fmt: off
+
+    def __init__(self) -> None:
+        """Initialize SoundBrowser."""
+
+        display.show(self.LOGO)
+        sleep(1_000)
+
+        self.index: int = 0
+        self.sounds: "list[str]" = [sound for sound in dir(Sound) if sound.isupper()]
+        self.sound: str = self.sounds[self.index]
+        self.size: int = len(self.sounds)
+
+        print("INFO: sounds=" + str(self.sounds))
+
+    def browse_left(self) -> None:
+        """Scroll carousel to the left."""
+
+        self.index = (self.index - 1) % self.size
+        self.sound = self.sounds[self.index]
+
+        self.show()
+
+    def browse_right(self) -> None:
+        """Scroll carousel to the right."""
+
+        self.index = (self.index + 1) % self.size
+        self.sound = self.sounds[self.index]
+
+        self.show()
+
+    def show(self) -> None:
+        """Show the current sound."""
+
+        print("INFO: sound: " + self.sound)
+        display.scroll(self.sound)
+
+    def play(self) -> None:
+        """Play the current sound."""
+
+        display.show(self.SOUND)
+        audio.play(getattr(Sound, self.sound))
 
 
-def browse_left(index: int) -> int:
-    """Scroll carousel to the left.
+print()
+print("Sound Browser")
+print()
 
-    :param index: the current position
-    :return: the new position
-    """
-    return (index - 1) % COUNT
-
-
-def browse_right(index: int) -> int:
-    """Scroll carousel to the right.
-
-    :param index: the current position
-    :return: the new position
-    """
-    return (index + 1) % COUNT
+app: SoundBrowser = SoundBrowser()
 
 
 # Code in a 'while True:' loop repeats forever
 while True:
-    display.show(Image.ARROW_E)
+    sleep(1_000)
+    display.show(app.BWD)
     sleep(500)
-    display.show(Image.ARROW_W)
+    display.show(app.FWD)
+    sleep(500)
+
+    app.show()
+    sleep(500)
+
+    app.play()
     sleep(500)
 
     if button_a.was_pressed():
-        index = browse_left(index)
+        app.browse_left()
     elif button_b.was_pressed():
-        index = browse_right(index)
-
-    sound: str
-
-    if index == -1:
-        sound = "SAD"
-    else:
-        sound = sounds[index]
-
-    print("INFO: sound: " + sound)
-    display.scroll(sound)
-    audio.play(getattr(Sound, sound))
-
-    sleep(1_000)
+        app.browse_right()
